0217000a28d76554649fc50875178e058f0f69eb
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-04-16 21:41:02 +0300

    covered role resolver by tests

--- src/main/java/org/geekhub/doctorsregistry/web/security/role/RoleResolver.java
+++ src/main/java/org/geekhub/doctorsregistry/web/security/role/RoleResolver.java
@@ -2,10 +2,8 @@ package org.geekhub.doctorsregistry.web.security.role;
 
 import org.geekhub.doctorsregistry.web.dto.clinic.CreateClinicUserDTO;
 import org.geekhub.doctorsregistry.web.dto.doctor.CreateDoctorUserDTO;
-import org.geekhub.doctorsregistry.web.dto.user.CreateUserDTO;
 import org.geekhub.doctorsregistry.web.dto.patient.CreatePatientUserDTO;
-import org.geekhub.doctorsregistry.web.security.role.Role;
-import org.geekhub.doctorsregistry.web.security.role.RoleNotSupportedException;
+import org.geekhub.doctorsregistry.web.dto.user.CreateUserDTO;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -14,6 +12,9 @@ import org.springframework.stereotype.Component;
 public class RoleResolver {
 
     public Role resolveRole(CreateUserDTO userDTO) {
+        if (userDTO == null) {
+            throw new IllegalArgumentException();
+        }
         if (userDTO instanceof CreateDoctorUserDTO) {
             return Role.DOCTOR;
         } else if (userDTO instanceof CreateClinicUserDTO) {
@@ -26,6 +27,9 @@ public class RoleResolver {
     }
 
     public Role resolveRole(UserDetails user) {
+        if (user == null) {
+            throw new IllegalArgumentException();
+        }
         if (user.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CLINIC"))) {
             return Role.CLINIC;
         } else if (user.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_DOCTOR"))) {
