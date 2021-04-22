afc09daaeeb091e9e8d864e8b7414ad1dbdcd57d
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-04-22 11:13:14 +0300

    added admin

---
+++ src/main/resources/db/migration/user/V20210422110813__add_admin.sql
@@ -0,0 +1,4 @@
+insert into user_credentials (email, password, enabled)
+values ('admin@gmail.com', '$2y$12$Hi.UXG1QeBTtfoSYe1UGROpKg46zYt.Df0PX/ePrqlR.ZpNcTKvES', true);
+insert into authority (email, authority_name)
+values ('admin@gmail.com', 'ROLE_ADMIN');
