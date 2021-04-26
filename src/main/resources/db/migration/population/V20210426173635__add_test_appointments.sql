6011587d47ef2dcccbc0e1b4b5429c277a722da8
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-04-26 17:43:30 +0300

    added test appointments

---
+++ src/main/resources/db/migration/population/V20210426173635__add_test_appointments.sql
@@ -0,0 +1,6 @@
+insert into appointment (patient_id, doctor_working_hour_id, date)
+values ('1', '1', '2021-04-19');
+insert into appointment (patient_id, doctor_working_hour_id, date)
+values ('3', '2', '2021-04-19');
+insert into appointment (patient_id, doctor_working_hour_id, date)
+values ('2', '2', '2021-04-19');
