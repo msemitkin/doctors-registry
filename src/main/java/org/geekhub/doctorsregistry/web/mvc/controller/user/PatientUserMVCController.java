0bd5060fee910144d536bea0201b8168ebd9d4f1
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-05-05 16:04:24 +0300

    added csrf token to appointment cancel form

--- src/main/java/org/geekhub/doctorsregistry/web/mvc/controller/user/PatientUserMVCController.java
+++ src/main/java/org/geekhub/doctorsregistry/web/mvc/controller/user/PatientUserMVCController.java
@@ -1,11 +1,8 @@
 package org.geekhub.doctorsregistry.web.mvc.controller.user;
 
 import org.geekhub.doctorsregistry.domain.appointment.AppointmentService;
-import org.geekhub.doctorsregistry.domain.mapper.AppointmentMapper;
 import org.geekhub.doctorsregistry.domain.patient.PatientService;
 import org.geekhub.doctorsregistry.web.dto.patient.CreatePatientUserDTO;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,35 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
-import java.util.stream.Collectors;
 
 @Controller
 @ApiIgnore
 public class PatientUserMVCController {
 
     private final PatientService patientService;
-    private final AppointmentMapper appointmentMapper;
     private final AppointmentService appointmentService;
 
     public PatientUserMVCController(
         PatientService patientService,
-        AppointmentMapper appointmentMapper,
         AppointmentService appointmentService
     ) {
         this.patientService = patientService;
-        this.appointmentMapper = appointmentMapper;
         this.appointmentService = appointmentService;
     }
 
     @GetMapping("/patients/me/cabinet")
-    public String cabinet(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-        Integer doctorId = patientService.getIdByEmail(userDetails.getUsername());
-        model.addAttribute("archive", patientService.getArchivedAppointments(doctorId).stream()
-            .map(appointmentMapper::toDTO)
-            .collect(Collectors.toList()));
-        model.addAttribute("pending", patientService.getPendingAppointments(doctorId).stream()
-            .map(appointmentMapper::toDTO)
-            .collect(Collectors.toList()));
+    public String cabinet() {
         return "patient-cabinet";
     }
 
