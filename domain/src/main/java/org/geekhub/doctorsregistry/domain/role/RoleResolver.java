fb8c0d4dd2922e2adb6cd098fc38fccb1abe3d2f
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-09-16 22:58:13 +0300

    T10 removed irrelevant code

--- domain/src/main/java/org/geekhub/doctorsregistry/domain/role/RoleResolver.java
+++ domain/src/main/java/org/geekhub/doctorsregistry/domain/role/RoleResolver.java
@@ -1,30 +1,11 @@
 package org.geekhub.doctorsregistry.domain.role;
 
-import org.geekhub.doctorsregistry.web.dto.clinic.CreateClinicUserDTO;
-import org.geekhub.doctorsregistry.web.dto.doctor.CreateDoctorUserDTO;
-import org.geekhub.doctorsregistry.web.dto.patient.CreatePatientUserDTO;
-import org.geekhub.doctorsregistry.web.dto.user.CreateUserDTO;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoleResolver {
-
-    public Role resolveRole(CreateUserDTO userDTO) {
-        if (userDTO == null) {
-            throw new IllegalArgumentException();
-        }
-        if (userDTO instanceof CreateDoctorUserDTO) {
-            return Role.DOCTOR;
-        } else if (userDTO instanceof CreateClinicUserDTO) {
-            return Role.CLINIC;
-        } else if (userDTO instanceof CreatePatientUserDTO) {
-            return Role.PATIENT;
-        } else {
-            throw new RoleNotSupportedException();
-        }
-    }
 
     public Role resolveRole(UserDetails user) {
         if (user == null) {
